a4061f441ebfc3c7bf301c6ca362343ed5afcd24
ThatzOkay <noduhgamingxd@gmail.com>
2025-04-30 16:18:01 +0200

    first commit

---
+++ src/types/carousel.d.ts
@@ -0,0 +1,6 @@
+declare module '@jambonn/vue-concise-carousel' {
+  import { DefineComponent } from 'vue';
+
+  export const Carousel: DefineComponent<object, object, unknown>;
+  export const Slide: DefineComponent<object, object, unknown>;
+}
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,16 @@
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-
+    <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+      <ion-refresher-content></ion-refresher-content>
+    </ion-refresher>
+    <Carousel paginationPosition="bottom-overlay" :paginationEnabled="true" :navigationEnabled="true"
+            paginationActiveColor="var(--color-primary)" paginationColor="var(--tw-prose-body)" class="h-full w-full"
+            :perPage="1" :loop="true">
+            <slide v-for="image of carouselImages" :key="image" class="h-full">
+              <img :src="image" class="mt-0 mb-0 h-full w-full object-cover" />
+            </slide>
+          </Carousel>
       <div id="container">
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
@@ -22,7 +31,31 @@
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRefresher, IonRefresherContent, onIonViewDidEnter } from '@ionic/vue';
+import { ref } from 'vue';
+import { Carousel, Slide } from '@jambonn/vue-concise-carousel';
+import '@jambonn/vue-concise-carousel/lib/vue-concise-carousel.css';
+
+const carouselImages = ref<string[] | null>(null)
+
+const carouselBaseData = [
+  "https://pngimg.com/uploads/pokemon/pokemon_PNG129.png",
+  "https://pngimg.com/uploads/pokemon/pokemon_PNG123.png"
+  ]
+
+  onIonViewDidEnter(() => {
+    carouselImages.value = carouselBaseData.map((image) => {
+      return image
+    })
+  })
+
+  const handleRefresh = (event: CustomEvent) => {
+    carouselImages.value = null;
+    setTimeout(() => {
+    carouselImages.value = [...carouselBaseData].sort(() => Math.random() - 0.5);
+    (event.target as any).complete();
+    }, 500);
+  }
 </script>
 
 <style scoped>
